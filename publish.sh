0ed84972c56be0677cbaa457ae66118839efc4b9
Chaozz <hzzhouchao89@gmail.com>
2016-10-13 18:53:07 -0400

    javadoc

---
+++ publish.sh
@@ -0,0 +1,14 @@
+git checkout gh-pages || exit $?
+
+# Clear out the old files:
+rm -rf javadoc/*
+# Replace them with new files and commit them:
+cp -pr build/docs/javadoc/ javadoc \
+&& git add javadoc \
+&& git commit -a -m "generated javadoc"
+ERROR=$?
+
+git checkout master || exit $?
+[ $ERROR -eq 0 ] || exit $ERROR
+git push origin master || exit $?
+git push origin gh-pages || exit $?
