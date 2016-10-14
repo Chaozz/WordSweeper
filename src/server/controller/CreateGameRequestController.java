bd9014a970b402894b5234def7ca06adfc9a7b3b
GuoTianhao <rggth09@gmail.com>
2016-10-14 16:04:13 -0400

    edit CreateController

--- src/server/controller/CreateGameRequestController.java
+++ src/server/controller/CreateGameRequestController.java
@@ -4,7 +4,7 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import server.ClientState;
 import server.IProtocolHandler;
-import server.model.ServerModel;
+import server.model.*;
 import xml.Message;
 
 /**
@@ -13,31 +13,41 @@ import xml.Message;
  */
 public class CreateGameRequestController implements IProtocolHandler {
 
-	ServerModel model;
-	
-	public CreateGameRequestController (ServerModel model) {
-		this.model = model;
-	}
-	
-	public Message process(ClientState client, Message request) {
-		
-		model.joinGame();  // HACK.
-		
-		// note you can retrieve information from the request...
-		Node createRequest = request.contents.getFirstChild();
-		NamedNodeMap map = createRequest.getAttributes();
-		
-		String pname = map.getNamedItem("name").getNodeValue();
-		
-		
-		// Construct message reflecting state
-		String xmlString = Message.responseHeader(request.id()) +
-				"<boardResponse gameId='hg12jhd' managingUser='" + pname + "' bonus='4,3' contents='ABCGBCJDH...HDJHJD'>" +
-			      "<player name='" + pname + "' score='392489038' position='4,6' board='AFERKSOEROIERPOR'/>" +
-			  "</boardResponse>" +
-			"</response>";
-		
-		// send this response back to the client which sent us the request.
-		return new Message (xmlString);
-	}
+    ServerModel model;
+
+    public CreateGameRequestController(ServerModel model) {
+        this.model = model;
+    }
+
+    public Message process(ClientState client, Message request) {
+
+        model.joinGame();  // HACK.
+
+        // note you can retrieve information from the request...
+        Node createRequest = request.contents.getFirstChild();
+        NamedNodeMap map = createRequest.getAttributes();
+
+        String pname = map.getNamedItem("name").getNodeValue();
+        String gameID = model.createGame(pname);
+        Game game = model.getGame(gameID);
+        Player me = game.getPlayer(pname);
+        Position origin = me.getOrigin();
+        Board board = game.getBoard();
+        Position multiplier = board.getMultiplier();
+
+
+        // Construct message reflecting state
+        String xmlString = Message.responseHeader(request.id()) +
+                "<boardResponse gameId='" + gameID + "' managingUser='" + pname + "' bonus='" + multiplier.getRow() +
+                "," + multiplier.getCol() + "' " +
+                "contents='" + board.getBoardContent() + "'>" +
+                "<player name='" + pname + "' score='" + me.getScore() + "' position='" + origin.getRow() + "," +
+                origin.getCol() + "' " +
+                "board='" + board.getLocalBoardContent(origin) + "'/>" +
+                "</boardResponse>" +
+                "</response>";
+
+        // send this response back to the client which sent us the request.
+        return new Message(xmlString);
+    }
 }
--- src/server/model/Board.java
+++ src/server/model/Board.java
@@ -11,6 +11,8 @@ public class Board {
             "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     public Hashtable<Position, Cell> cells = new Hashtable<Position, Cell>();
     int size;
+    StringBuffer content;
+
 
     /**
      * construct the Board with fixed sized and random letters.
@@ -19,22 +21,20 @@ public class Board {
      */
     public Board(int size) {
         this.size = size;
-        Random r = new Random();
-        for (int i = 0; i < this.size; i++) {
-            for (int j = 0; j < this.size; j++) {
-                cells.put(new Position(i, j), new Cell(new Letter(alphabet[r.nextInt(alphabet.length)], 0), false));
-            }
-        }
+        resetBoard();
     }
 
     /**
      * reset the board.
      */
     public void resetBoard() {
+        content = new StringBuffer();
         Random r = new Random();
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
-                cells.put(new Position(i, j), new Cell(new Letter(alphabet[r.nextInt(alphabet.length)], 0), false));
+                String rAlphabet = alphabet[r.nextInt(alphabet.length)];
+                content.append(rAlphabet);
+                cells.put(new Position(i, j), new Cell(new Letter(rAlphabet, 0), false));
             }
         }
     }
@@ -49,5 +49,32 @@ public class Board {
 
     public void setCells(Position position, Cell cell) {
         cells.put(position, cell);
+    }
+
+    public Position getMultiplier(){
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                Position position = new Position(i,j);
+                if (cells.get(position).isMultiplier()){
+                    return position;
+                }
+            }
+        }
+        return null;
+    }
+
+    public String getLocalBoardContent(Position position){
+        content = new StringBuffer();
+        for (int i = position.getRow(); i < 4+position.getRow(); i++) {
+            for (int j = position.getCol(); j < 4+position.getCol(); j++) {
+                Position p = new Position(i,j);
+                content.append(cells.get(new Position(i,j)).getLetter().getCharacter());
+            }
+        }
+        return content.toString();
+    }
+
+    public String getBoardContent(){
+        return content.toString();
     }
 }
