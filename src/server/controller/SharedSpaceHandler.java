735e55d8a5eb92986e4ad84c43f86fff06c6ee92
GuoTianhao <rggth09@gmail.com>
2016-12-09 17:39:34 -0500

    shared space player num

--- src/server/controller/SharedSpaceHandler.java
+++ src/server/controller/SharedSpaceHandler.java
@@ -1,16 +1,33 @@
 package server.controller;
 
 import server.model.Game;
+import server.model.Player;
 import server.model.Position;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 /**
  * Created by tianhao on 12/9/16.
  */
 public class SharedSpaceHandler {
-    public static int numOfSharedPlayers(Game game, Position position){
-        int num = 0;
-
-        return num;
+    public static int numOfSharedPlayers(Game game, Position position) {
+        Hashtable<Position, Integer> sharedNum = new Hashtable<>();
+        ArrayList<Player> players = game.getPlayers();
+        for (Player p : players) {
+            Position origin = p.getOrigin();
+            for (int i = origin.getRow(); i < origin.getRow() + 4; i++) {
+                for (int j = origin.getCol(); j < origin.getCol() + 4; j++) {
+                    Position temp = new Position(i, j);
+                    if (sharedNum.containsKey(temp)) {
+                        sharedNum.put(temp, sharedNum.get(temp) + 1);
+                    } else {
+                        sharedNum.put(temp, 1);
+                    }
+                }
+            }
+        }
+        return sharedNum.get(position);
     }
 
 }
