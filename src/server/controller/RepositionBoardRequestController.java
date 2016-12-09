328239186138061663840356373f043108ed019c
GuoTianhao <rggth09@gmail.com>
2016-12-09 16:50:56 -0500

    add todo controllers

---
+++ src/server/controller/RepositionBoardRequestController.java
@@ -0,0 +1,7 @@
+package server.controller;
+
+/**
+ * Created by tianhao on 12/9/16.
+ */
+public class RepositionBoardRequestController {
+}
---
+++ src/server/controller/SharedSpaceHandler.java
@@ -0,0 +1,16 @@
+package server.controller;
+
+import server.model.Game;
+import server.model.Position;
+
+/**
+ * Created by tianhao on 12/9/16.
+ */
+public class SharedSpaceHandler {
+    public static int numOfSharedPlayers(Game game, Position position){
+        int num = 0;
+
+        return num;
+    }
+
+}
