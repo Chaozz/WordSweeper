3547009ce21028c3a34f791a1c240c50d73db34d
cissymtt <mtt2000@126.com>
2016-12-13 17:02:11 -0500

    add lockgame test

---
+++ test/server/controller/TestLockGameRequestController.java
@@ -0,0 +1,54 @@
+package server.controller;
+
+import org.w3c.dom.NamedNodeMap;
+
+import server.MockClient;
+import server.Server;
+import server.model.ServerModel;
+import xml.Message;
+import junit.framework.TestCase;
+
+// validate that server generates appropriate modelResponse for each modelRequest.
+public class TestLockGameRequestController extends TestCase {
+
+    /**
+     * Become a placeholder for responses sent to the (only connected) client.
+     */
+    MockClient client1;
+
+    ServerModel model;
+
+    protected void setUp() {
+        // FIRST thing to do is register the protocol being used.
+        if (!Message.configure("wordsweeper.xsd")) {
+            fail("unable to configure protocol");
+        }
+
+        client1 = new MockClient();
+        Server.register("c1", client1);
+        model = new ServerModel();
+    }
+
+    protected void tearDown() {
+        Server.unregister("c1");
+        Server.unregister("c2");
+    }
+
+    public void testSimple() {
+        // client1 creates game
+        String xmlString = Message.requestHeader() + "<createGameRequest name='test'/></request>";
+        Message request = new Message(xmlString);
+
+        // get response after processing this request
+        Message response = new CreateGameRequestController(model).process(client1, request);
+
+        assertTrue(response.success());
+
+        NamedNodeMap map = response.contents.getFirstChild().getAttributes();
+        String gameID=map.getNamedItem("gameId").getNodeValue();
+        xmlString = Message.requestHeader() + "<lockGameRequest gameId='"+gameID+"'/></request>";
+        request = new Message(xmlString);
+        response=new LockGameRequestController(model).process(client1,request);
+        assertTrue(response.success());
+    }
+}
